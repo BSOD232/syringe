de87d25b10bbe09b050f78799f4331780289f037
xSkripting <92183387+xSkripting@users.noreply.github.com>
2022-10-30 17:55:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,43 @@ var keyHandler = function (event) {
 // Listen for keydown events
 document.addEventListener('keydown', keyHandler, false);
 
+
+function listener(event) {
+  switch(event.type) {
+    case "animationend":
+      console.log(`Ended: elapsed time is ${event.elapsedTime}`)
+      let foo = document.getElementById("box");
+      foo.remove();
+      document.getElementById('blank').style.animation="leftpanel 2s";
+      
+      
+      
+      
+      break;
+
+  }
+}
+
+const element = document.getElementById("box");
+element.addEventListener("animationend", listener, false);
+element.className = "box";
+
+
+
+function listener2(event) {
+  switch(event.type) {
+    case "animationend":
+      console.log(`Ended: elapsed time is ${event.elapsedTime}`)
+      let foe = document.getElementById("blank");
+      foe.remove();
+      
+      
+      
+      break;
+
+  }
+}
+
+const element2 = document.getElementById("blank");
+element2.addEventListener("animationend", listener2, false);
+element2.className = "blank";
